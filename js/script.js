59647ed979db4cc735c56033eb5d26ffd772063a
unknown <https://github.com/KIrillweb2023/Humanking.git>
2023-06-20 14:50:51 +0300

    new test

--- js/script.js
+++ js/script.js
@@ -1,63 +1,70 @@
 let config = {
     number: 0,
-    progressСhecking: 0,
     defaultUnitLevel: 0
 };
 
-const progressUnitLevel = {
-    unitOne: 100,
-    unitTwo: 200,
-    unitThree: 500,
-    unitFor: 700,
-    unitFive: 1000
-
+const configProgress = {
+    progress: 1000
 }
+
+
 
 const clickNum = document.getElementById('#click-score');
 const level = document.getElementById('#click-EXP');
-
 const section = document.querySelector('.promo');
-
-console.log(clickNum);
+const progress = document.querySelector('progress');
 
 
 section.addEventListener('click', (e) =>{
     const target = e.target;
     config.number += 1;
+    showMoney();
 
-    console.log(config.number);
     if(target && target.classList.contains('promo') || target.classList.contains('promo__wrapper')){
-        config.progressСhecking += 5;
-        calculationLevel();
-
-        console.log(config.progressСhecking);
+        levelFarm();
     }
+
     level.innerHTML = config.defaultUnitLevel;
     clickNum.innerHTML = config.number;
 });
 
 
-function calculationLevel(){
-    if(config.progressСhecking == progressUnitLevel.unitOne){
-        config.defaultUnitLevel += 1;
-    } else if(config.progressСhecking == progressUnitLevel.unitTwo){
-        config.defaultUnitLevel += 1;
-    } else if(config.progressСhecking == progressUnitLevel.unitThree){
-        config.defaultUnitLevel += 1;
-    } else if(config.progressСhecking == progressUnitLevel.unitFor){
-        config.defaultUnitLevel += 1;
-    } else if(config.progressСhecking == progressUnitLevel.unitFive){
-        config.defaultUnitLevel += 1;
+function showMoney(){
+
+
+    if(config.defaultUnitLevel == 2){
+        config.number += 4;
+    } else if(config.defaultUnitLevel > 3){
+        config.number += 9;
+    }
+}
+function levelFarm(){
+    if(progress.value != configProgress.progress){
+        progress.value += 2;
+        if(progress.value == configProgress.progress){
+            config.defaultUnitLevel += 1;
+        } else {
+            config.defaultUnitLevel += 0;
+        }
+    } else {
+        progress.value = 0;
     }
 }
 
+// menu
 
+const open = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const close = document.querySelector('.close');
 
-
-
-
-
-// get settings
+open.addEventListener('click', () =>{
+    menu.classList.add('active');
+    document.body.style.overflow = 'hidden';
+});
+close.addEventListener('click', () =>{
+    menu.classList.remove('active');
+    document.body.style.overflow = '';
+});
 
 
 
