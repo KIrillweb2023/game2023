fdb3aa37e7e410219cfb4b8b5b65ee439f0aac87
unknown <https://github.com/KIrillweb2023/Humanking.git>
2023-06-20 19:00:01 +0300

    update 20.06.2023 21:00

--- js/script.js
+++ js/script.js
@@ -56,13 +56,16 @@ function levelFarm(){
 const open = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const close = document.querySelector('.close');
+const overlay = document.querySelector('.menu__overlay');
 
 open.addEventListener('click', () =>{
     menu.classList.add('active');
+    overlay.classList.add('active');
     document.body.style.overflow = 'hidden';
 });
 close.addEventListener('click', () =>{
     menu.classList.remove('active');
+    overlay.classList.remove('active');
     document.body.style.overflow = '';
 });
 
