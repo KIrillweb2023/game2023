4b9bb2f6809eab618f3ab759d4203ce474987ab6
unknown <https://github.com/KIrillweb2023/Humanking.git>
2023-06-21 16:54:59 +0300

    update 0.5

--- js/script.js
+++ js/script.js
@@ -3,9 +3,6 @@ let config = {
     defaultUnitLevel: 0
 };
 
-const configProgress = {
-    progress: 1000
-}
 
 
 
@@ -14,42 +11,23 @@ const level = document.getElementById('#click-EXP');
 const section = document.querySelector('.promo');
 const progress = document.querySelector('progress');
 
+const moneyProfile = document.getElementById('#money-profile');
+
 
 section.addEventListener('click', (e) =>{
     const target = e.target;
     config.number += 1;
-    showMoney();
+
 
     if(target && target.classList.contains('promo') || target.classList.contains('promo__wrapper')){
-        levelFarm();
     }
 
-    level.innerHTML = config.defaultUnitLevel;
     clickNum.innerHTML = config.number;
 });
 
 
-function showMoney(){
 
 
-    if(config.defaultUnitLevel == 2){
-        config.number += 4;
-    } else if(config.defaultUnitLevel > 3){
-        config.number += 9;
-    }
-}
-function levelFarm(){
-    if(progress.value != configProgress.progress){
-        progress.value += 2;
-        if(progress.value == configProgress.progress){
-            config.defaultUnitLevel += 1;
-        } else {
-            config.defaultUnitLevel += 0;
-        }
-    } else {
-        progress.value = 0;
-    }
-}
 
 // menu
 
